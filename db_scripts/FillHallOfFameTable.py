999d46a49cc72a2cf6e158f9268d240589ced43c
svenihde <sven.ihde@student.hpi.de>
2016-05-08 22:25:00 +0200

    Add python script to fill the HallOfFame table with the static content.

---
+++ db_scripts/FillHallOfFameTable.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""Take HallOfFame and save it to the database"""
+
+import json
+import psycopg2
+
+with open('db_config') as file:
+    HOST = file.readline().strip()
+    DBNAME = file.readline().strip()
+    USER = file.readline().strip()
+    PASS = file.readline().strip()
+
+with open('data/hall_of_fame.json') as file:
+    HOF = json.load(file)
+
+def fill_repeatable_table():
+    """Main function"""
+
+    conn_string = "host=" + HOST + " dbname=" + DBNAME + " user=" + USER + " password=" + PASS
+    conn = psycopg2.connect(conn_string)
+    cursor = conn.cursor()
+    query = "INSERT INTO halloffame(name, description, pointsfirst, pointssecond, pointsthird) VALUES (%s,%s,%s,%s,%s);"
+
+    for item in HOF:
+        data = (item["description"], item["description"], item["firstPlace"], item["secondPlace"], item["thirdPlace"])
+        cursor.execute(query, data)
+
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+if __name__ == '__main__':
+    fill_repeatable_table()
