24e78a958485717cca0b7a14dd3ad344998c31e3
Florian Richter <florian.richter@sap.com>
2016-05-10 00:21:31 +0200

    Rename file to satisfy pylint

--- db_scripts/GetRecentMatches.py
+++ db_scripts/GetRecentMatches.py
@@ -4,9 +4,9 @@
 import json
 import psycopg2
 import requests
-import player_champion
 
-import UpdatePoints as db_up
+import PlayerChampion
+import UpdatePoints
 
 with open('key') as file:
     KEY = {'api_key': file.readline().strip()}
@@ -45,14 +45,15 @@ def get_recent_matches(player_id, region):
         match_id = match["gameId"]
         query = "SELECT id, playerid, region FROM game WHERE id ='" + str(match_id) + "' AND playerid = '" + str(player_id) + "' AND region = '" + region + "';"
         cursor.execute(query)
-       
+
         if cursor.rowcount == 0: #new match found
             query = "INSERT INTO game(id, playerid, region, json) VALUES (%s,%s,%s,%s);"
             data = (match_id, player_id, region, json.dumps(match))
-            db_up.calculate_points(player_id, match_id, region, match)
             cursor.execute(query, data)
             conn.commit()
-            player_champion.update(match_id, player_id, region, match)
+
+            UpdatePoints.calculate_points(player_id, match_id, region, match)
+            PlayerChampion.update(match_id, player_id, region, match)
 
     cursor.close()
     conn.close()
