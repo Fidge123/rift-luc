d3d6f9277fd26b434bd01f5f44030dc929663b13
svenihde <sven.ihde@student.hpi.de>
2016-05-09 12:14:19 +0200

    Add funtion to get all recent matches for all the players in the database

--- db_scripts/GetRecentMatches.py
+++ db_scripts/GetRecentMatches.py
@@ -14,11 +14,23 @@ with open('db_config') as file:
     USER = file.readline().strip()
     PASS = file.readline().strip()
 
+def get_all_recent_matches():
+    conn_string = "host=" + HOST + " dbname=" + DBNAME + " user=" + USER + " password=" + PASS
+    conn = psycopg2.connect(conn_string)
+    cursor = conn.cursor()
+    query = "SELECT id, region FROM player;"
+    cursor.execute(query)
+    players = cursor.fetchall()
+    for player in players:
+        player_id = player[0]
+        region = player[1]
+        get_recent_matches(player_id,region)
+    
 def get_recent_matches(player_id, region):
 
     api = 'https://' + region + '.api.pvp.net/api/lol/' + region
 
-    matches = json.loads(requests.get(api + '/v1.3/game/by-summoner/' + player_id + '/recent', params=KEY).text)["games"]
+    matches = json.loads(requests.get(api + '/v1.3/game/by-summoner/' + str(player_id) + '/recent', params=KEY).text)["games"]
     
     conn_string = "host=" + HOST + " dbname=" + DBNAME + " user=" + USER + " password=" + PASS
     conn = psycopg2.connect(conn_string)
@@ -26,7 +38,7 @@ def get_recent_matches(player_id, region):
     
     for match in matches:
         match_id = match["gameId"]
-        query = "SELECT id, playerid, region FROM game WHERE id ='" + str(match_id) + "' AND playerid = '" + player_id + "' AND region = '" + region + "';"
+        query = "SELECT id, playerid, region FROM game WHERE id ='" + str(match_id) + "' AND playerid = '" + str(player_id) + "' AND region = '" + region + "';"
         cursor.execute(query)
         if cursor.rowcount == 0: #new match found
             query = "INSERT INTO game(id, playerid, region, json) VALUES (%s,%s,%s,%s);"
