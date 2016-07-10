a6fa63452f2dabec147c072093dc548d6661407b
Florian Richter <florian.richter@sap.com>
2016-07-10 16:33:59 +0200

    Refactor main.py

--- db_scripts/main.py
+++ db_scripts/main.py
@@ -7,44 +7,56 @@ import GetRecentMatches
 import RegisterPlayer
 
 def usage():
+    """Show usage help"""
     print("Schema: main.py --update \n" +
           "main.py --register --name='name' --region='region' " +
           "--emai='email' --password='password' --leagueid='leagueid' \n" +
           "main.py --register -n 'name' -r 'region -e 'email' -p 'password' " +
           "-l 'leagueid'")
-    
-if len(argv) == 1:
-    print("Please give an argument or use main.py -h/--help for options")
-try:
-    opts,args = getopt.getopt(argv[1:],"hn:r:e:p:l:",
-                              ["help", "update","register","name=","region=",
-                               "email","password=","leagueid="])
-except getopt.GetoptError as err:
-    print(err)
-    usage()
-register = False
-for o, a in opts:
-    if o in ("-h","--help"):
-        usage()
-    elif o == "--update":
-        GetRecentMatches.get_all_recent_matches()
-    elif o == "--register":
-        register = True
-if register:
-    name, region, email, password, leagueid = ''
-    for o,a in opts:
-        if o in ("-n","--name"):
-            name = a
-        elif o in ("-r","--region"):
-            region = a
-        elif o in ("-e","--email"):
-            email = a
-        elif o in ("-p","--password"):
-            password = a
-        elif o in ("-l","--leagueid"):
-            leagueid = a
+
+def register(opts):
+    """Register a new player"""
+    name = region = email = password = leagueid = ''
+
+    for opt, arg in opts[0]:
+        if opt in ("-n", "--name"):
+            name = arg
+        elif opt in ("-r", "--region"):
+            region = arg
+        elif opt in ("-e", "--email"):
+            email = arg
+        elif opt in ("-p", "--password"):
+            password = arg
+        elif opt in ("-l", "--leagueid"):
+            leagueid = arg
+
     if name and region and password and leagueid and email:
-        RegisterPlayer.register_player(name,region,email,password,leagueid)
+        RegisterPlayer.register_player(name, region, email, password, leagueid)
     else:
         usage()
-    
+
+def main():
+    """Run requested command"""
+
+    if len(argv) == 1:
+        print("Please give an argument or use main.py -h/--help for options")
+
+    try:
+        opts = getopt.getopt(argv[1:], "hn:r:e:p:l:",
+                             ["help", "update", "register", "name=", "region=",
+                              "email=", "password=", "leagueid="])
+
+        for opt in opts[0]:
+            if opt[0] in ("-h", "--help"):
+                usage()
+            elif opt[0] == "--update":
+                GetRecentMatches.get_all_recent_matches()
+            elif opt[0] == "--register":
+                register(opts)
+
+    except getopt.GetoptError as err:
+        print(err)
+        usage()
+
+if __name__ == '__main__':
+    main()
