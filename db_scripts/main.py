37806a8ec5635ac805f72c2114bfb140a8e33527
svenihde <sven.ihde@student.hpi.de>
2016-07-10 18:36:29 +0200

    Add reset funtion to main file, to reset db

--- db_scripts/main.py
+++ db_scripts/main.py
@@ -3,16 +3,58 @@
 
 from sys import argv
 import getopt
+import psycopg2
 import GetRecentMatches
 import RegisterPlayer
+import FillAchievementTable, FillChampionTable, FillHallOfFameTable, FillRepeatableTable
+
+with open('db_config') as file:
+    HOST = file.readline().strip()
+    DBNAME = file.readline().strip()
+    USER = file.readline().strip()
+    PASS = file.readline().strip()
 
 def usage():
     """Show usage help"""
     print("Schema: main.py --update \n" +
+          "main.py --reset --region='region/-r 'region' \n" +
           "main.py --register --name='name' --region='region' " +
           "--emai='email' --password='password' --leagueid='leagueid' \n" +
           "main.py --register -n 'name' -r 'region -e 'email' -p 'password' " +
           "-l 'leagueid'")
+
+def delete():
+    conn_string = "host=" + HOST + " dbname=" + DBNAME + " user=" + USER + " password=" + PASS
+    conn = psycopg2.connect(conn_string)
+    cur = conn.cursor()
+    query = "DROP SCHEMA public CASCADE;"
+    cur.execute(query)
+    query = "CREATE SCHEMA public;"
+    cur.execute(query)
+    conn.commit()
+    cur.close()
+    conn.close()
+
+def create():
+    conn_string = "host=" + HOST + " dbname=" + DBNAME + " user=" + USER + " password=" + PASS
+    conn = psycopg2.connect(conn_string)
+    with conn.cursor() as cursor:
+        cursor.execute(open("DatabaseCreation.sql", "r").read())
+    conn.commit()
+    conn.close()
+
+def fill(opts):
+    region = 'euw'
+    for opt, arg in opts[0]:
+        if opt in ("-r", "--region"):
+            region = arg
+    if not region:
+        usage()
+        return
+    FillAchievementTable.fill_achievement_table()
+    FillChampionTable.fill_champion_table(region)
+    FillHallOfFameTable.fill_hall_of_fame_table()
+    FillRepeatableTable.fill_repeatable_table()
 
 def register(opts):
     """Register a new player"""
@@ -44,7 +86,7 @@ def main():
     try:
         opts = getopt.getopt(argv[1:], "hn:r:e:p:l:",
                              ["help", "update", "register", "name=", "region=",
-                              "email=", "password=", "leagueid="])
+                              "email=", "password=", "leagueid=", "reset"])
 
         for opt in opts[0]:
             if opt[0] in ("-h", "--help"):
@@ -53,6 +95,11 @@ def main():
                 GetRecentMatches.get_all_recent_matches()
             elif opt[0] == "--register":
                 register(opts)
+            elif opt[0] == "--reset":
+                delete()
+                create()
+                fill(opts)
+                
 
     except getopt.GetoptError as err:
         print(err)
