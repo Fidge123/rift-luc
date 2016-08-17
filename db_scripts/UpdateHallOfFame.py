70da74830d3246474af4f3df792227b661ed8631
Florian Richter <florian@lkr24.de>
2016-08-18 00:38:20 +0200

    Fix bug in HOF_UPDATE query

--- db_scripts/UpdateHallOfFame.py
+++ db_scripts/UpdateHallOfFame.py
@@ -9,7 +9,7 @@ with open("db_config") as file:
     USER = file.readline().strip()
     PASS = file.readline().strip()
 
-HOF_UPDATE = "UPDATE player_halloffame SET (playerid, region) = (%s,%s) WHERE hofid = %s AND place = %S;"
+HOF_UPDATE = "UPDATE player_halloffame SET (playerid, region) = (%s,%s) WHERE hofid = %s AND place = %s;"
 
 def update():
     """Update Hall of Fame data"""
