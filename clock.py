1f33dd2e3911ea619d704e07ffddd602b386c7b0
svenihde <sven.ihde@student.hpi.de>
2017-04-23 16:09:44 +0200

    Add startup to clock.py

--- clock.py
+++ clock.py
@@ -1,8 +1,10 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
 import os
 
+os.system("src/main.py --reset")
 sched = BlockingScheduler()
 
 @sched.scheduled_job('interval', minutes=10)
 def timed_job():
     os.system("src/main.py --update")
+    os.system("src/main.py --verify")
