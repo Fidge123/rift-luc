197cf77ab62684c6a949561fb9128fd47d580fc1
svenihde <sven.ihde@student.hpi.de>
2017-04-23 18:01:00 +0200

    replace replace with sub

--- clock.py
+++ clock.py
@@ -1,14 +1,15 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
 import os
 from os import environ
+import re
 
 file = open('key', 'w+')
 file.write(environ["KEY"])
 
 DATABASE_NAME = environ["DATABASE_URL"].split("/")[-1]
 with open("src/DatabaseCreation.sql", "r+") as myfile:
-    data = myfile.read().replace("\n","")
-    data.replace("luc", DATABASE_NAME)
+    data = myfile.read()
+    data = re.sub("luc", DATABASE_NAME, data)
     myfile.seek(0)
     myfile.write(data)
     myfile.truncate()
